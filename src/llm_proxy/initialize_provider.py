ca0c6fe9b899ff6ba7924bb498c1270198cb1022
Alberto Gonzalez Olmos <alberto.gonzalez.olmos@sallinggroup.com>
2024-08-05 15:29:25 +0200

    starting of async functionality to enable the usage of providers in tandem

--- src/llm_proxy/initialize_provider.py
+++ src/llm_proxy/initialize_provider.py
@@ -1,12 +1,13 @@
 from groq import Groq
 from together import Together
 
-# from openai import OpenAI
+import asyncio
 from dotenv import load_dotenv
 import os
 from typing import Literal, Union
 from abc import ABC
 import datetime
+from time import perf_counter
 import logging
 
 from io import BytesIO
@@ -23,16 +24,16 @@ class LlmProxy(ABC):
         self.provider = provider
         self.modality = modality
         self.model = model
-        self.client_initialize(self.provider, self.model)
+        self.client_initialize()
         self.request_per_minute_limit_initialize()
         match modality:
             case "chat":
                 self.tokenizer_initialize()
 
-    def client_initialize(self, provider: list_providers) -> Union[Together, Groq]:
+    def client_initialize(self) -> Union[Together, Groq]:
         load_dotenv()
 
-        match provider:
+        match self.provider:
             case "groq":
                 api_key = os.environ.get("GROQ_API_KEY")
                 self.client = Groq(api_key=api_key)
@@ -47,12 +48,12 @@ class LlmProxy(ABC):
                         # "mixtral-8x7b-32768"
                         # "gemma-7b-it"
                     case "image":
-                        return self.error_not_supported()
+                        return self.error_modality_not_supported()
                     case "stt":
-                        return self.error_not_supported()
+                        return self.error_modality_not_supported()
                         # self.model = "whisper-large-v3"
                     case "code":
-                        return self.error_not_supported()
+                        return self.error_modality_not_supported()
 
             case "together":
                 api_key = os.environ.get("TOGETHER_API_KEY")
@@ -76,9 +77,9 @@ class LlmProxy(ABC):
                             # "stabilityai/stable-diffusion-xl-base-1.0"
                             # "wavymulder/Analog-Diffusion"
                     case "stt":
-                        return self.error_not_supported()
+                        return self.error_modality_not_supported()
                     case "code":
-                        return self.error_not_supported()
+                        return self.error_modality_not_supported()
                         # "Qwen/Qwen1.5-72B"
                         # "mistralai/Mistral-7B-v0.1"
                         # "mistralai/Mixtral-8x7B-v0.1"
@@ -88,14 +89,16 @@ class LlmProxy(ABC):
         )
 
     def logging_initialize(self) -> None:
-        self.log = logging.getLogger(__name__)
-        self.log.basicConfig(
-            level=logging.DEBUG,
+
+        logging.basicConfig(
+            level=logging.INFO,
             format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
         )
+        self.log = logging.getLogger(__name__)
+        self.log.info("Starting LLM Proxy...")
 
-    def error_not_supported(self) -> None:
+    def error_modality_not_supported(self) -> None:
         self.log.error(f"Modality: '{self.modality}' is not supported by '{self.provider}'.")
 
     def request_per_minute_limit_initialize(self) -> None:
@@ -111,15 +114,25 @@ class LlmProxy(ABC):
             case "together":
                 self.request_per_minute_limit = 60
 
+    async def rpm_wait(self) -> bool:
+        print(f"will await {60 / self.request_per_minute_limit} seconds ...")
+        time_before = perf_counter()
+        await asyncio.sleep(60 / self.request_per_minute_limit)
+        print(f"... waited: {perf_counter() - time_before} seconds.")
+        return True
+
     def tokenizer_initialize(self) -> None:
         self.session_start = llm_proxy_time_string()
         self.llm_messages_count_tokens = 0
         self.llm_response_count_tokens = 0
 
-    def count_tokens(self, llm_message: str = "", llm_response: str = "") -> None:
-        if llm_message and llm_response:
-            self.llm_messages_count_tokens += len(llm_message.split())
-            self.llm_response_count_tokens += len(llm_response.split())
+    async def count_async_tokens(self, llm_message: str, llm_response: str) -> None:
+        print("count!")
+        await asyncio.to_thread(self.count_tokens, llm_message, llm_response)
+
+    def count_tokens(self, llm_message: str, llm_response: str) -> None:
+        self.llm_messages_count_tokens += len(llm_message.split())
+        self.llm_response_count_tokens += len(llm_response.split())
 
     def get_token_count(self) -> list[int, int]:
         token_count_dict = {}
@@ -148,6 +161,18 @@ class LlmProxy(ABC):
         im.save("picture_out.jpg", "JPEG")
 
         return image_out
+
+    async def get_async_completion(
+        self,
+        system_prompt: str = "",
+        user_prompt: str = "",
+        temperature: int = 0,
+    ) -> str:
+
+        if await self.rpm_wait():
+            llm_response = await asyncio.to_thread(self.get_completion, system_prompt, user_prompt, temperature)
+            await self.count_async_tokens(system_prompt + user_prompt, llm_response)
+        return llm_response
 
     def get_completion(
         self,
@@ -193,12 +218,6 @@ class LlmProxy(ABC):
 
                 llm_response = completion.content[0].text
 
-        llm_message_result_list = [
-            ", ".join([f"{key}: {value}" for key, value in dictionary.items()]) for dictionary in llm_message
-        ]
-        llm_message_result_string = ", ".join(llm_message_result_list)
-        self.count_tokens(llm_message_result_string, llm_response)
-
         return llm_response
 
 
@@ -209,7 +228,23 @@ def llm_proxy_time_string() -> str:
     return formatted_string
 
 
+list_tandem_models = Literal["llama-3.1-70b", "llama-3.1-8b"]
+
+
+class TandemProxy(ABC):
+    def __init__(self, model: list_tandem_models):
+        self.logging_initialize()
+        self.model = model
+        self.client_initialize()
+        self.request_per_minute_limit_initialize()
+        self.tokenizer_initialize()
+
+    def client_initialize(self) -> Union[Together, Groq]:
+        load_dotenv()
+
+
 def main():
+
     shopping_list = f"""
 
     4 apples, 
@@ -220,14 +255,39 @@ def main():
     200 g walnuts,
 
     """
-    provider = "groq"
-    model = "llama-3.1-70b-versatile"
-    llmObj = LlmProxy(provider=provider, model=model)
-
+    provider = "together"
+    modality = "chat"
+    model = ""
+    llmObj = LlmProxy(provider=provider, modality=modality, model=model)
+    print("start")
     completion = llmObj.get_completion(user_prompt=shopping_list)
-
+    print("finish")
     print(completion)
 
 
+async def main_async():
+    shopping_list = f"""
+
+    4 apples, 
+    500g chicken,
+    2 apples,
+    100 chicken,
+    1 kg carrots,
+    200 g walnuts,
+
+    """
+    provider = "together"
+    modality = "chat"
+    model = ""
+    llmObj = LlmProxy(provider=provider, modality=modality, model=model)
+    print("start")
+    completion = await llmObj.get_async_completion(user_prompt=shopping_list)
+    print("finish")
+    print(completion)
+    print("finish finish")
+
+
 if __name__ == "__main__":
+    # asyncio.run(main_async())
+
     main()
